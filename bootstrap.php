62f312e6d927308ff537d272217a6ee3f1959748
Santosh <santosh@readybytes.in>
2022-02-24 13:49:56 +0530

    Database connector added

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 use Dotenv\Dotenv;
+use Src\System\DatabaseConnector;
 
 $dotenv = new DotEnv(__DIR__);
 $dotenv->load();
+$dbConnection = (new DatabaseConnector())->getConnection();
